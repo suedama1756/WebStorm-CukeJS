5fc61abb81cca1dcdc97480762bb6087f72b18f9
Jason Young <jyoung1756@gmail.com>
2014-06-05 22:42:38 +0100

    Renamed to directory to keep inline with other plugins

--- src/com/jdml/cucumber/js/execution/CucumberRunConfigurationParametersSerializer.java
+++ src/com/jdml/cucumber/js/execution/CucumberRunConfigurationParametersSerializer.java
@@ -7,35 +7,35 @@ import org.jetbrains.annotations.NotNull;
 
 public class CucumberRunConfigurationParametersSerializer
 {
-    private static final String FEATURE_PATH_KEY = "feature-path";
+    private static final String FEATURE_DIRECTORY_KEY = "feature-directory";
     private static final String ARGUMENTS_KEY = "arguments";
-    private static final String CUCUMBER_PATH_KEY = "cucumber-path";
+    private static final String CUCUMBER_DIRECTORY_KEY = "cucumber-directory";
 
     private CucumberRunConfigurationParametersSerializer() {}
 
     @NotNull
     public static void readInto(@NotNull Element parent, @NotNull CucumberRunConfigurationParameters parameters)
     {
-        String featurePath = readTag(parent, FEATURE_PATH_KEY);
+        String featurePath = readTag(parent, FEATURE_DIRECTORY_KEY);
         parameters.setFeaturePath(FileUtil.toSystemDependentName(featurePath));
 
         String arguments = readTag(parent, ARGUMENTS_KEY);
         parameters.setArguments(arguments);
 
-        String cucumberPath = readTag(parent, CUCUMBER_PATH_KEY);
+        String cucumberPath = readTag(parent, CUCUMBER_DIRECTORY_KEY);
         parameters.setCucumberPath(FileUtil.toSystemDependentName(cucumberPath));
     }
 
     public static void write(@NotNull Element parent, @NotNull CucumberRunConfigurationParameters parameters)
     {
         String featurePath = FileUtil.toSystemIndependentName(parameters.getFeaturePath());
-        writeTag(parent, FEATURE_PATH_KEY, featurePath);
+        writeTag(parent, FEATURE_DIRECTORY_KEY, featurePath);
 
         String arguments = parameters.getArguments();
         writeTag(parent, ARGUMENTS_KEY, arguments);
 
         String cucumberPath = FileUtil.toSystemIndependentName(parameters.getCucumberPath());
-        writeTag(parent, CUCUMBER_PATH_KEY, cucumberPath);
+        writeTag(parent, CUCUMBER_DIRECTORY_KEY, cucumberPath);
     }
 
     @NotNull
